ff199a23e8ee55cd844dbf26251824bca6e97180
GowthamKN03 <kngowtham7008@gmail.com>
2024-01-01 22:04:11 +0530

    Commit_Message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+ import Project1 from './Project1';
+
+export default function App() {
+   const myList = [
+     { id: 'a', value: 'apple' },
+     { id: 'b', value: 'banana' },
+     { id: 'c', value: 'orange' },
+     { id: 'd', value: 'grapes' },
+     { id: 'e', value: 'mango' },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Project1 myList={myList}/>
+      
     </div>
   );
 }
-
-export default App;
